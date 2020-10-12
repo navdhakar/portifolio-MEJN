4d4a4c246593c75dd183b5bafde52cbc950d4da7
navdhakar <navdepdhakar1@gmail.com>
2020-10-12 09:40:24 +0530

    projecs nav added

--- client/projects.js
+++ client/projects.js
@@ -1,18 +1,26 @@
 /** @format */
 
 const navl = document.getElementById('navl');
-var proj = document.getElementById('projects');
+var prj = document.getElementById('projects');
 var prl = document.createElement('div');
+var projec = document.getElementById('projec');
+var lang = document.getElementById('lang');
 navl.style.width = '50px';
 navl.style.height = '150px';
 navl.addEventListener('mouseover', exp);
-
+projec.style.display = 'none';
+prj.style.display = '';
 function exp() {
   navl.style.width = '1200px';
   navl.style.height = '700px';
   navl.style.marginLeft = '200px';
 
-  navl.style.marginTop = '-350px';
+  navl.style.marginTop = '-500px';
+  prj.style.display = 'none';
+
+  projec.style.display = '';
+  navl.style.overflow = 'hidden';
+  lang.style.display = 'none';
 }
 navl.addEventListener('mouseout', shr);
 function shr() {
@@ -20,7 +28,9 @@ function shr() {
   navl.style.height = '150px';
   navl.style.marginTop = '-120px';
   navl.style.marginLeft = '1320px';
-  proj.style.display = 'none';
+  prj.style.display = '';
+  projec.style.display = 'none';
+  lang.style.display = '';
 }
 // navl.addEventListener('mouseout', shr);
 // function shr() {
